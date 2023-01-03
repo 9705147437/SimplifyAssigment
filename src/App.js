81c5af961063bcc633b7ad59bf8640849c8b8869
9705147437 <sudheerseshapu@gmail.com>
2023-01-03 12:33:58 +0530

    Changes commited

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+       <div className='rt1'></div>
+       <div className='rt2'></div>
+       <div className='rt3'></div>
+       <div className='rt4'></div>
+       <div className='ptext'><h1>HTML<span>&</span>CSS</h1><p>design and build websites</p></div>
+    </div>
+  );
+}
+
+export default App;
